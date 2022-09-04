7cf4814dfb7c05fc074006242bc4bc3942d4eda5
ChristosIoannou <christos.ioannou@outlook.com>
2022-09-04 12:43:43 +0100

    using fftw instead of kiss

--- dancingMesh/src/ofApp.cpp
+++ dancingMesh/src/ofApp.cpp
@@ -1,5 +1,7 @@
 #include "ofApp.h"
 
+//#define OFX_FFT_USE_FFTW
+
 //--------------------------------------------------------------
 void ofApp::setup() {
 
@@ -7,11 +9,12 @@ void ofApp::setup() {
     ofSetCircleResolution(80);
     ofBackground(54, 54, 54);
 
-    int bufferSize = N*2; // 512
+    int bufferSize = N*4; // 256
 
-    fft = ofxFft::create(bufferSize, OF_FFT_WINDOW_BARTLETT);
+    fft = ofxFft::create(bufferSize, OF_FFT_WINDOW_HAMMING, OF_FFT_FFTW);
     audioInput = new float[bufferSize];
-    fftOutput = new float[fft->getBinSize()];
+    //fftOutput = new float[fft->getBinSize()];
+    fftOutput = new float[N];
 
     ofSoundStreamSettings settings;
 
@@ -38,6 +41,7 @@ void ofApp::setup() {
     soundStream.setup(settings);
 
     spectrum.resize(N, 0.0f);
+    soundSpectrum.resize(N, 0.0f);
     p.resize(n);
 
     for (int i = 0; i < n; ++i) {
@@ -52,19 +56,10 @@ void ofApp::setup() {
 
 //--------------------------------------------------------------
 void ofApp::update() {
-    //ofSoundUpdate();
-    //Get current spectrum with N bands
-    //float* val = ofSoundGetSpectrum(N);
 
-    //We should not release memory of val,
-    //because it is managed by sound engine
-    //Update our smoothed spectrum,
-    //by slowly decreasing its values and getting maximum with val
-    //So we will have slowly falling peaks in spectrum
-    //for (int i = 0; i < N; i++) {
-    //    spectrum[i] *= 0.98;	//Slow decreasing
-    //    spectrum[i] = max(spectrum[i], fftOutput[i]);
-    //}
+    //soundMutex.lock();
+    spectrum = soundSpectrum;
+    //soundMutex.unlock();
 
     //Update particles using spectrum values
     //Computing dt as a time between the last
@@ -92,9 +87,6 @@ void ofApp::update() {
     //Calculate color based on range
     analyseFFT();
 
-    //if (ofGetFrameNum() % 60 == 0) {
-    //    std::cout << "fps: " << ofGetFrameRate() << std::endl;
-    //}
 }
 
 //--------------------------------------------------------------
@@ -134,8 +126,11 @@ void ofApp::draw() {
         for (int i = 0; i < N; i++) {
             //Draw bandRad and bandVel by black color,
             //and other by gray color
-            if (i == bandRad || i == bandVel) {
-                ofSetColor(ofColor::orange); //Black color
+            if (i == bandRad) {
+                ofSetColor(ofColor::red); //Black color
+            }
+            else if (i == bandVel) {
+                ofSetColor(ofColor::green);
             }
             else {
                 ofSetColor(ofColor::lightGoldenRodYellow); //Gray color
@@ -202,6 +197,18 @@ void ofApp::keyPressed(int key) {
     case 'f':
         ofToggleFullscreen();
         break;
+    case 'w':
+        bandRad++;
+        break;
+    case 'q':
+        bandRad--;
+        break;
+    case 's':
+        bandVel++;
+        break;
+    case 'a':
+        bandVel--;
+        break;
     default:
         break;
     }
@@ -212,7 +219,9 @@ void ofApp::audioIn(ofSoundBuffer& input) {
     
     audioInput = input.getBuffer().data();
     fft->setSignal(audioInput);
-    memcpy(fftOutput, fft->getAmplitude(), sizeof(float) * fft->getBinSize());
+    //memcpy(fftOutput, fft->getAmplitude(), sizeof(float) * fft->getBinSize());
+
+    memcpy(fftOutput, fft->getAmplitude(), sizeof(float) * N);
 
     for (int i = 0; i < N; i++) {
         fftOutput[i] = fftOutput[i] * (volumeMultiplier / 4);
@@ -223,40 +232,13 @@ void ofApp::audioIn(ofSoundBuffer& input) {
     ////Update our smoothed spectrum,
     ////by slowly decreasing its values and getting maximum with val
     ////So we will have slowly falling peaks in spectrum
+    //soundMutex.lock();
     for (int i = 0; i < N; i++) {
-        spectrum[i] *= 0.98;	//Slow decreasing
-        spectrum[i] = max(spectrum[i], fftOutput[i]);
+        soundSpectrum[i] *= 0.99;	//Slow decreasing
+        soundSpectrum[i] = max(soundSpectrum[i], fftOutput[i]);
     }
+    //soundMutex.unlock();
 
-    ////Update particles using spectrum values
-    ////Computing dt as a time between the last
-    ////and the current calling of update()
-    //float time = ofGetElapsedTimef();
-    //float dt = time - time0;
-    //dt = ofClamp(dt, 0.0, 0.1);
-    //time0 = time; //Store the current time
-
-    ////Update Rad and Vel from spectrum
-    ////Note, the parameters in ofMap's were tuned for best result
-    ////just for current music track
-    //Rad = ofMap(spectrum[bandRad], 1, 3, 500, 1200, true);
-    //Vel = ofMap(spectrum[bandVel], 0, 0.1, 0.05, 0.5);
-    ////Update particles positions
-    //for (int j = 0; j < n; j++) {
-    //    tx[j] += Vel * dt;	//move offset
-    //    ty[j] += Vel * dt;	//move offset
-    //    //Calculate Perlin's noise in [-1, 1] and
-    //    //multiply on Rad
-    //    p[j].x = ofSignedNoise(tx[j]) * Rad;
-    //    p[j].y = ofSignedNoise(ty[j]) * Rad;
-    //}
-
-    ////Calculate color based on range
-    //analyseFFT();
-
-    ////if (ofGetFrameNum() % 60 == 0) {
-    ////    std::cout << "fps: " << ofGetFrameRate() << std::endl;
-    ////}
 }
 
 //--------------------------------------------------------------
